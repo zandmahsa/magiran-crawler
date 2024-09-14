5183dcb3281adfe0b12f2b1450e20ffa09e99402
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 11:06:48 -1200

    Update crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,17 +4,21 @@ import re
 import json
 from time import sleep
 from urllib.parse import unquote
-all_links=[]
 
+# Variables
+all_links=[]
 dley= 60
 dley2 = 3
+
+# Load links and JSON data
 with open('./links.txt') as f:
     for i in f:
         all_links.append(i.strip('\n'))
+	    
 with open('json.json','r',encoding='utf-8-sig') as f:
 	js = json.loads(f.readline())
 
-#all_links=['https://www.magiran.com/author/%d9%85%d8%ad%d9%85%d8%af%20%d9%85%d9%86%d8%a7%d9%86%20%d8%b1%d8%a6%db%8c%d8%b3%db%8c']
+# HTTP headers
 headers = {
       'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0',
       'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -25,13 +29,14 @@ headers = {
       'Connection': 'keep-alive',
       'TE': 'Trailers',
      }
+
 paper = 'https://www.magiran.com/paper/'
 s = requests.Session()
 s.headers.update(headers)
-data = {"a":"","coId":"","p":2,"name":""}
+data = {"a":"", "coId":"", "p":2, "name":""}
 
 
-
+# Main scraping loop
 for i in all_links:
     try:
         sleep(dley2)
@@ -45,7 +50,8 @@ for i in all_links:
     else:
         is_load = True ###
     name = unquote(i).split('/')[-1]
-    
+
+    # Pagination if more info is available
     if is_load:
         for j in range (2,100):
             try:
@@ -59,15 +65,22 @@ for i in all_links:
                 links+=links_n
             except Exception as E:
                 print(E)
+    # Data structure for author and articles
     dict0={'نام':name,'مقالات':[]}
+
+    # Scrape each article
     for ii in links:
         try:
             sleep(dley2)
             res = s.get(paper+str(ii))
             so =BeautifulSoup(res.text,'html.parser')
             ti=so.select('.py-2')[0].text
+		
+	    # Initialize article details
             key,athor,entesharat,lang,typem,pages='','','','','',''
             athor=so.select('.paper-info > div:nth-child(1) > div:nth-child(2)')[0].text.replace('\n','')
+
+		
             for i in range(2,11):
                 try:
                     ttt=so.select('div.row:nth-child('+str(i)+') > div:nth-child(1)')[0].text
@@ -98,6 +111,7 @@ for i in all_links:
                 js['res'].append(dict0)
         except Exception as E:
             print(E)
+    # Save the updated data
     with open('json.json','w',encoding='utf8') as f:
         f.write(json.dumps(js,ensure_ascii=False))
     sleep(dley)
